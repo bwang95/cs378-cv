44f66d8cf73f8262c7641a1df09e126e9bd9b455
bwang95 <b.wang@utexas.edu>
2014-03-25 13:21:58 -0500

    Pull conflicts

--- FeatureDetection/src/main.cpp
+++ FeatureDetection/src/main.cpp
@@ -11,43 +11,47 @@ using namespace cv;
 void readme();
 
 /** @function main */
-int main( int argc, char** argv )
-{
-  if( argc != 3 )
-  { readme(); return -1; }
+int main( int argc, char **argv ) {
+	if ( argc != 3 ) {
+		readme();
+		return -1;
+	}
 
-  Mat img_1 = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE );
-  Mat img_2 = imread( argv[2], CV_LOAD_IMAGE_GRAYSCALE );
+	Mat img_1 = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE );
+	Mat img_2 = imread( argv[2], CV_LOAD_IMAGE_GRAYSCALE );
 
-  if( !img_1.data || !img_2.data )
-  { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
+	if ( !img_1.data || !img_2.data ) {
+		std::cout << " --(!) Error reading images " << std::endl;
+		return -1;
+	}
 
-  //-- Step 1: Detect the keypoints using SURF Detector
-  int minHessian = 400;
+	//-- Step 1: Detect the keypoints using SURF Detector
+	int minHessian = 400;
 
-  SurfFeatureDetector detector( minHessian );
+	SurfFeatureDetector detector( minHessian );
 
-  std::vector<KeyPoint> keypoints_1, keypoints_2;
+	std::vector<KeyPoint> keypoints_1, keypoints_2;
 
-  detector.detect( img_1, keypoints_1 );
-  detector.detect( img_2, keypoints_2 );
+	detector.detect( img_1, keypoints_1 );
+	detector.detect( img_2, keypoints_2 );
 
-  //-- Draw keypoints
-  Mat img_keypoints_1; Mat img_keypoints_2;
+	//-- Draw keypoints
+	Mat img_keypoints_1; Mat img_keypoints_2;
 
-  drawKeypoints( img_1, keypoints_1, img_keypoints_1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
-  drawKeypoints( img_2, keypoints_2, img_keypoints_2, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+	drawKeypoints( img_1, keypoints_1, img_keypoints_1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+	drawKeypoints( img_2, keypoints_2, img_keypoints_2, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
 
-  //-- Show detected (drawn) keypoints
-  imshow("Keypoints 1", img_keypoints_1 );
-  imshow("Keypoints 2", img_keypoints_2 );
+	//-- Show detected (drawn) keypoints
+	imshow("Keypoints 1", img_keypoints_1 );
+	imshow("Keypoints 2", img_keypoints_2 );
 
-  waitKey(0);
+	waitKey(0);
 
-  return 0;
-  }
+	return 0;
+}
 
-  /** @function readme */
-  void readme()
-  { std::cout << " Usage: ./SURF_detector <img1> <img2>" << std::endl; }
+/** @function readme */
+void readme() {
+	std::cout << " Usage: ./SURF_detector <img1> <img2>" << std::endl;
+}
 
