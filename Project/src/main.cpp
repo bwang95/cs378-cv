ce9f3743dc83967f1dd04d2f2d53c4a4b73fec08
bwang95 <b.wang@utexas.edu>
2014-03-27 15:27:06 -0500

    Reformatted code

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -4,28 +4,26 @@
 
 using namespace std;
 
-int main(int argc, char **argv){
-	
+int main(int argc, char **argv) {
+
 	Directory *dir = new Directory("ImgDB");
 	int size = dir->getSize();
-        cout<<"Number of files in directory: "<<size<<endl;
+	cout << "Number of files in directory: " << size << endl;
 	//getSize retrieves the number of objects in the directory
 	//makeList() creates the list pointer array and returns true if successful
-	if(dir->makeList())
-	{
-		for(int i = 0; i<size;i++)
-			cout<<dir->list[i]<<" ";
-	}
-	else
-		cout<<"Could not open directory."<<endl;
-	cout<<endl;
+	if (dir->makeList()) {
+		for (int i = 0; i < size; i++)
+			cout << dir->list[i] << " ";
+	} else
+		cout << "Could not open directory." << endl;
+	cout << endl;
 
-	if(argc < 3){
+	if (argc < 3) {
 		cout << "Project <img1> <img2> (optional: downsize)" << endl;
 		return 1;
 	}
 	FeatureMatcher *matcher = new FeatureMatcher(argv[1], argv[2]);
-	if(argc >= 4)
+	if (argc >= 4)
 		matcher -> setDownsize(atoi(argv[3]));
 	matcher -> drawFeatures();
 
