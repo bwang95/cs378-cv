22052cb99801235e299eeb9c0c807be433db0574
bwang95 <b.wang@utexas.edu>
2014-04-22 15:34:09 -0500

    New gitignore

---
+++ FileIndexer.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Collections;
+import java.util.HashSet;
+
+/**
+ * Created by brian on 4/17/14.
+ */
+public class FileIndexer {
+    static PrintStream out;
+    static HashSet<String> acceptedExtensions;
+
+    public static void main(String... args) throws IOException {
+        out = new PrintStream(args.length >= 1 ? args[0] : "FileIndex.txt");
+        File file = new File(".");
+
+        String[] ext = {"jpg", "png", "jpeg", "bmp"};
+
+        acceptedExtensions = new HashSet<String>();
+        Collections.addAll(acceptedExtensions, ext);
+
+        explore(file);
+    }
+
+    public static void explore(File root) throws IOException {
+        if (root == null || root.listFiles() == null)
+            return;
+        for (File f : root.listFiles()) {
+            if (f == null)
+                continue;
+            if (f.isDirectory())
+                explore(f);
+            else if (acceptedExtensions.contains(getExtension(f.getName()))){
+                System.out.println(f.getPath());
+                out.println(f.getPath());
+            }
+        }
+    }
+
+    private static String getExtension(String name) {
+        return name.substring(name.lastIndexOf(".") + 1).toLowerCase();
+    }
+}
