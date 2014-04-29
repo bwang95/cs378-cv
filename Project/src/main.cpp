6e5adc655ead5ee0d008ed6a76a1c9b7c23822e0
ajm4482 <andymedina@utexas.edu>
2014-04-29 15:48:40 -0500

    commit

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -25,7 +25,7 @@ void *computeMatches(void *threadid){
 	min = id * NUM_THREADS;
 	max = min + size/NUM_THREADS - 1;
 	cout<<"Thread: "<<id<<endl;
-	if(size % NUM_THREADS =! 0 && id == (NUM_THREADS-1))
+	if(size % NUM_THREADS != 0 && id == (NUM_THREADS-1))
 		max = size-1;
 	FeatureMatcher matcher(path, dir.list[min].c_str());
 	for (int i = min + 1; i < max; i++) 
